dca177826d985b044b28b5c79ea5028f7f7f5979
Mballer7 <103282381+Mballer7@users.noreply.github.com>
2022-04-11 10:20:15 -0700

    Update main.java

--- main.java
+++ main.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class calculatorProject {
     public static void main(String[] args) {
         Scanner userInput1 = new Scanner(System.in);
-        System.out.println("___________________________");
+        System.out.println("______CALCULATOR MENU______");
         System.out.println("| 1) Pythagorean Theorem  |");
         System.out.println("| 2) Discounting          |");
-        System.out.println("|                         |");
+        System.out.println("| 3) Price Conversion     |");
         System.out.println("|                         |");
         System.out.println("|                         |");
         System.out.println("|                         |");
@@ -36,6 +36,35 @@ public class calculatorProject {
             endPrice = (s*total)/100;
             System.out.print("Total after discount: "+ endPrice);
 
+        }
+        
+        if ( selection == 3){
+                double u = 0.79;
+                double e = 0.73;
+                double p = 0.61;
+                double a = 1.07;
+                double b = 3.73;
+                Scanner UI = new Scanner(System.in);
+                System.out.println("1.USD 2.EUROS 3.Pounds 4.Australian 5.Brazilian Real");
+                System.out.println("What do you want to convert from CAD?");
+                int conv = UI.nextInt();
+                System.out.println("How much CAD do you want to convert?");
+                double amount = UI.nextInt();
+                if (conv == 1){
+                    System.out.println(amount + " CAD is " +  amount*u + " USD");
+                }
+                if (conv == 2){
+                    System.out.println(amount + " CAD is " +  amount*e + " Euros");
+                }
+                if (conv == 3){
+                    System.out.println(amount + " CAD is " +  amount*p + " Pounds");
+                }
+                if (conv == 4){
+                    System.out.println(amount + " CAD is " +  amount*a + " AUS");
+                }
+                if (conv == 5){
+                    System.out.println(amount + " CAD is " +  amount*b + " Reals");
+                }
         }
     }
 }
